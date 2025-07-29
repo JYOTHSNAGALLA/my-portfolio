c3d9e270771b96bf1aca6c18e6ad91f287d69408
JYOTHSNAGALLA <gallajyothsna13@gmail.com>
2025-07-29 23:00:26 +0530

    Initial commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -48,7 +48,7 @@ const About = () => {
 
         <h3 className="text-2xl font-semibold mt-8 mb-4">Clients & Collaborations</h3>
         <div className="flex flex-wrap gap-4 justify-center">
-          <img src="/profile.jpg" alt="Profile" />
+          <img src={profilePic} alt="Profile" className="rounded-full w-32 h-32" />
           <img src={clientLogo} alt="Client Logo" className="h-12" />
 
         </div>
