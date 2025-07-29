c67ea7079490759c18cb627c696502f99104676a
JYOTHSNAGALLA <gallajyothsna13@gmail.com>
2025-07-29 22:46:09 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 
 // Replace 'your-username' and 'repo-name' with your GitHub details
 export default defineConfig({
-  base: '/portfolio-site/',
+  base: '/my-portfolio/',
   plugins: [react()],
 });
