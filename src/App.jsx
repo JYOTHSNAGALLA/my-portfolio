3dc909440809031f718f8bdc9d5d6ebc2ee7ba23
JYOTHSNAGALLA <gallajyothsna13@gmail.com>
2025-07-30 17:41:23 +0530

    Fix MobileNav import error

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import Sidebar from "./components/Sidebar";
 import MobileNav from "./components/MobileNav";
+import DesktopNavbar from "./components/DesktopNavbar";
+import About from "./components/About";
+import Resume from "./components/Resume";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div>
+    <div className="min-h-screen bg-gray-100 text-gray-800">
       <Sidebar />
       <MobileNav />
-      <main className="pt-20 md:ml-64 bg-gray-900 text-white min-h-screen">
-        {/* Add sections here */}
+      <DesktopNavbar />
+      <main className="pt-20 md:pl-72">
         <About />
         <Resume />
         <Projects />
