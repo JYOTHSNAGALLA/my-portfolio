edbbf01fc603cbe0d9a62d8558434232c7faabc1
JYOTHSNAGALLA <gallajyothsna13@gmail.com>
2025-07-30 11:55:27 +0530

    Fix About.jsx Vite config for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
+import React from 'react';
 import Sidebar from './components/Sidebar';
 import MobileNav from './components/MobileNav';
+import About from './components/About';
+import Resume from './components/Resume';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
-function App() {
+const App = () => {
   return (
     <div className="flex">
       <Sidebar />
       <div className="flex-1 ml-72 md:ml-72">
         <MobileNav />
-        {/* Your other components here */}
       </div>
     </div>
   );
 };
 
-export default App;
+export default App; // ✅ Make sure this line exists!
