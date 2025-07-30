654be2c073078cfd50d4dd9d36b60007541d3af2
JYOTHSNAGALLA <gallajyothsna13@gmail.com>
2025-07-30 11:57:47 +0530

    Fix About.jsx Vite config for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Sidebar from './components/Sidebar';
 import MobileNav from './components/MobileNav';
+import About from './components/About';
+import Resume from './components/Resume';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 const App = () => {
   return (
@@ -8,6 +12,12 @@ const App = () => {
       <Sidebar />
       <div className="flex-1 ml-72 md:ml-72">
         <MobileNav />
+        <main className="pt-20 space-y-20">
+          <About />
+          <Resume />
+          <Projects />
+          <Contact />
+        </main>
       </div>
     </div>
   );
